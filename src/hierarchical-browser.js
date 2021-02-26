11b24faf7a23169e83bf0a9fe0c8b155c8e564d0
tiny-george <jorge.franco@magnolia-cms.com>
2021-02-26 16:20:37 +0100

    Introduce some 'variable' in the template

--- src/hierarchical-browser.js
+++ src/hierarchical-browser.js
@@ -3,6 +3,7 @@ export default class HierarchicalBrowser extends HTMLElement {
     connectedCallback() {
         const template = document.getElementById('hierarchical-template');
         const instance = document.importNode(template.content, true);
+        instance.querySelector('.workspace-name').innerHTML = 'my-workspace';
         this.appendChild(instance);
     }
 }
