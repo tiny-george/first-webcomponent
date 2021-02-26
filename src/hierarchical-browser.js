ffdd91c2539a2b21620be361197b201a738dd9bd
Jorge Franco <jorge@MacBook-Pro.local>
2021-02-26 17:56:38 +0100

    Shadow dom in web component

--- src/hierarchical-browser.js
+++ src/hierarchical-browser.js
@@ -1,10 +1,15 @@
 export default class HierarchicalBrowser extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
+    }
 
     connectedCallback() {
+        const { shadowRoot } = this;
         const template = document.getElementById('hierarchical-template');
         const instance = document.importNode(template.content, true);
         instance.querySelector('.workspace-name').innerHTML = 'my-workspace';
-        this.appendChild(instance);
+        shadowRoot.appendChild(instance);
     }
 }
 
