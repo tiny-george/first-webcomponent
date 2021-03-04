4d5567d3fad2abd971bbd35aabb572220168ccb4
tiny-george <jorge.franco@magnolia-cms.com>
2021-03-04 19:48:13 +0100

    Add a attribute state and detect changing it

--- src/hierarchical-browser.js
+++ src/hierarchical-browser.js
@@ -1,15 +1,44 @@
 export default class HierarchicalBrowser extends HTMLElement {
     constructor() {
         super();
+        this.state = 'loading';
         this.attachShadow({mode: 'open'});
     }
 
+    get state() {
+        return this.getAttribute('state');
+    }
+
+    set state(value) {
+        this.setAttribute('state', value);
+    }
+
+    static get observedAttributes() { return ['state']; }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+       this.render();
+   }
+
     connectedCallback() {
-        const { shadowRoot } = this;
+        this.render();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = '';
+        if (this.getAttribute('state') == "loading") {
+            this.shadowRoot.appendChild(this.loading());
+            setTimeout(() => this.state = 'error' , 200);
+        }
+        if (this.getAttribute('state') == "error") {
+            this.shadowRoot.innerHTML = '<p>Error<p>';
+        }
+    }
+
+    loading() {
         const template = document.getElementById('hierarchical-template');
         const instance = document.importNode(template.content, true);
         instance.querySelector('.workspace-name').innerHTML = 'my-workspace';
-        shadowRoot.appendChild(instance);
+        return instance;
     }
 }
 
